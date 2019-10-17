24b408124e0f1d33c8479bd7f788e0d9ceb056ec
hanyoung <nastyo8dak@gmail.com>
2019-10-17 19:43:02 +0900

    #2.8
    Express Core: Routing

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello from my ass");
 
 // arrow function
@@ -25,4 +22,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
---
+++ init.js
@@ -0,0 +1,8 @@
+import app from "./app";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`😌Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
