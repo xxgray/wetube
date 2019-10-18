0143a93a540ea613c5a781864c977fb5484ac6a7
hanyoung <nastyo8dak@gmail.com>
2019-10-18 19:24:08 +0900

    #2.21
    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "hanyoung",
+      email: "nastyo8dak@gmail.com"
+    }
+  },
+  {
+    id: 960223,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "hanyoung",
+      email: "nastyo8dak@gmail.com"
+    }
+  },
+  {
+    id: 424214,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "hanyoung",
+      email: "nastyo8dak@gmail.com"
+    }
+  },
+  {
+    id: 346436,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "hanyoung",
+      email: "nastyo8dak@gmail.com"
+    }
+  }
+];
