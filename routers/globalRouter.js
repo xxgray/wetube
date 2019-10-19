c12c3426f053238e181f53280397da5ee38a10ce
hanyoung <nastyo8dak@gmail.com>
2019-10-19 17:04:52 +0900

    #2.24
    Log In and User Profile Controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,24 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
-import { getJoin, login, logout, postJoin } from "../controller/userController";
+import {
+  getJoin,
+  getLogin,
+  logout,
+  postJoin,
+  postLogin
+} from "../controller/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
